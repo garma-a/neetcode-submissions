cb32063d3cf86845eb41bc5df57c1c4fe06910bb
Garma (girgis emad) <126956967+garma-a@users.noreply.github.com>
2026-04-18 14:10:45 +0200

    Add: binary-tree-diameter - submission-8

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-8.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    diameterOfBinaryTree(root) {
+        let maxdepth = 0 ;
+        const dfs = (root)=>{
+            if(!root)return 0
+            let left = dfs(root.left) ,right= dfs(root.right);
+            maxdepth = Math.max(maxdepth ,left+right );
+            return Math.max(left , right)+1;
+            }
+            dfs(root)
+            return maxdepth
+    }
+   
+}
