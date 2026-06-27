22843cae0db57580db4a85e80d19e55510eb4fbd
Garma (girgis emad) <126956967+garma-a@users.noreply.github.com>
2026-06-27 05:53:57 +0300

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        if(nums.length===0)return 0;
+        let st = new Set(nums);
+        let ans = 1;
+        for (let num of nums) {
+            let curNum = num,
+                counter = 1;
+            while (st.has(curNum - 1)) {
+                counter++;
+                curNum -=1;
+            }
+            ans = Math.max(ans, counter);
+        }
+        return ans;
+    }
+}
